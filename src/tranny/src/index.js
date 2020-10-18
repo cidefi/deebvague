a5bfad090632d631b9c1c142b37a79d97015eecd
dtrash <dtrash@suicide.finance>
2020-10-18 00:53:26 -0400

    i am pretty high and have never seen react code before. am i winning?

--- src/tranny/src/index.js
+++ src/tranny/src/index.js
@@ -15,6 +15,8 @@ class AudioSegment {
 
   playAudio() {
     // play the dang audio file
+
+    // should be at a slow speed by default
   }
 }
 
@@ -25,7 +27,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
